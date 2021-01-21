24942407aad23a5275ce2420d563e1937d804431
phong100401 <65002403+phong100401@users.noreply.github.com>
2021-01-21 12:43:52 +0700

    LAb9-C#
    LAb9-C#

---
+++ EFCore/EFCore/Program.cs
@@ -0,0 +1,35 @@
+﻿using EFCore.Models;
+using System;
+
+namespace EFCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            insertProduct();
+             
+            Console.WriteLine("Press any key to continue");
+            Console.ReadKey();
+        }
+
+        static void insertProduct()
+        {
+            using (var db = new EFContext())
+            {
+                Product product = new Product();
+                product.Name = "Pen Drive";
+                db.Add(product);
+
+                product = new Product();
+                product.Name = "Memory Card";
+                db.Add(product);
+
+                db.SaveChanges();
+            }
+            return;
+        }
+
+    }
+}
